a890901508b9020c2a1442197b14728688868804
robagn <rob>
2014-10-23 14:37:03 +0200

    rinominato lo spider

---
+++ unifi/spiders/unifi_spider.py
@@ -0,0 +1,19 @@
+import scrapy
+#from __future__ import absolute_import 
+from unifi.items import UnifiItem
+
+class UnifiSpider(scrapy.Spider):
+	name="unifi_it"
+	allowed_domains = ["unifi.it"]
+	start_urls = ["http://www.unifi.it/vp-9333-scuole.html"]        
+	
+	def parse(self, response):
+		filename = response.url.split("/")[-2]
+# 		with open(filename, 'wb') as f:
+#  			f.write(response.body)
+		for sel in response.xpath('//ul/li'):
+ 			item=UnifiItem()
+ 			item['name'] = sel.xpath('a/text()').extract()
+ 			item['url'] = sel.xpath('a/@href').extract()
+ 			item['ref'] = sel.xpath('text()').re('-\s[^\n]*\\r')
+ 			yield item
